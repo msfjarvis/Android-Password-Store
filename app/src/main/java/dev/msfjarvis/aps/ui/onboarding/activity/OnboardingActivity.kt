eb8961b0d60c7426bd553bf9f42da87338beb010
Harsh Shandilya <me@msfjarvis.dev>
2021-06-14 14:49:26 +0530

    app: add a sample test of SimpleStorage use
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/aps/ui/onboarding/activity/OnboardingActivity.kt
+++ app/src/main/java/dev/msfjarvis/aps/ui/onboarding/activity/OnboardingActivity.kt
@@ -7,8 +7,14 @@ package dev.msfjarvis.aps.ui.onboarding.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import dev.msfjarvis.aps.R
 
+/**
+ * [OnboardingActivity] is annotated with [AndroidEntryPoint] because its child fragments use Hilt
+ * and it requires that [AndroidEntryPoint] fragments be attached to [AndroidEntryPoint] activities.
+ */
+@AndroidEntryPoint
 class OnboardingActivity : AppCompatActivity(R.layout.activity_onboarding) {
 
   override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/dev/msfjarvis/aps/ui/onboarding/fragments/RepoLocationFragment.kt
+++ app/src/main/java/dev/msfjarvis/aps/ui/onboarding/fragments/RepoLocationFragment.kt
@@ -5,22 +5,30 @@
 
 package dev.msfjarvis.aps.ui.onboarding.fragments
 
-import android.content.Intent
+import android.Manifest
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
+import com.anggrayudi.storage.SimpleStorage
+import com.anggrayudi.storage.callback.StorageAccessCallback
+import com.anggrayudi.storage.file.StorageType
+import com.anggrayudi.storage.file.getStorageId
 import com.github.ajalt.timberkt.d
 import com.github.ajalt.timberkt.e
 import com.github.michaelbull.result.onFailure
 import com.github.michaelbull.result.runCatching
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dagger.hilt.android.AndroidEntryPoint
 import dev.msfjarvis.aps.R
 import dev.msfjarvis.aps.data.repo.PasswordRepository
 import dev.msfjarvis.aps.databinding.FragmentRepoLocationBinding
-import dev.msfjarvis.aps.ui.settings.DirectorySelectionActivity
+import dev.msfjarvis.aps.injection.context.SAFHelperFactory
 import dev.msfjarvis.aps.util.extensions.finish
 import dev.msfjarvis.aps.util.extensions.getString
 import dev.msfjarvis.aps.util.extensions.listFilesRecursively
@@ -31,13 +39,14 @@ import dev.msfjarvis.aps.util.extensions.viewBinding
 import dev.msfjarvis.aps.util.settings.PasswordSortOrder
 import dev.msfjarvis.aps.util.settings.PreferenceKeys
 import java.io.File
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
 
+  @Inject lateinit var safHelperFactory: SAFHelperFactory
+  private lateinit var storage: SimpleStorage
   private val settings by unsafeLazy { requireActivity().applicationContext.sharedPrefs }
-  private val directorySelectIntent by unsafeLazy {
-    Intent(requireContext(), DirectorySelectionActivity::class.java)
-  }
   private val binding by viewBinding(FragmentRepoLocationBinding::bind)
   private val sortOrder: PasswordSortOrder
     get() = PasswordSortOrder.getSortOrder(settings)
@@ -49,22 +58,68 @@ class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
       }
     }
 
-  private val externalDirectorySelectAction =
-    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-      if (result.resultCode == AppCompatActivity.RESULT_OK) {
-        if (checkExternalDirectory()) {
-          finish()
-        } else {
-          createRepository()
-        }
+  private val requestStoragePermission =
+    registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+      if (granted) {
+        storage.requestStorageAccess(REQUEST_CODE_STORAGE_ACCESS)
       }
     }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding.hidden.setOnClickListener { createRepoInHiddenDir() }
-
     binding.sdcard.setOnClickListener { createRepoFromExternalDir() }
+    storage = safHelperFactory.create(requireActivity()).storage
+    if (savedInstanceState != null) {
+      storage.onRestoreInstanceState(savedInstanceState)
+    }
+    storage.storageAccessCallback =
+      object : StorageAccessCallback {
+        override fun onRootPathNotSelected(
+          requestCode: Int,
+          rootPath: String,
+          uri: Uri,
+          selectedStorageType: StorageType,
+          expectedStorageType: StorageType
+        ) {
+          MaterialAlertDialogBuilder(requireContext())
+            .setMessage("Please select $rootPath")
+            .setNegativeButton(android.R.string.cancel) { _, _ -> }
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+              val initialRoot =
+                if (expectedStorageType.isExpected(selectedStorageType)) selectedStorageType
+                else expectedStorageType
+              storage.requestStorageAccess(
+                REQUEST_CODE_STORAGE_ACCESS,
+                initialRoot,
+                expectedStorageType
+              )
+            }
+            .show()
+        }
+
+        override fun onCanceledByUser(requestCode: Int) {
+          Toast.makeText(requireContext(), "Canceled by user", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onRootPathPermissionGranted(requestCode: Int, root: DocumentFile) {
+          Toast.makeText(
+              requireContext(),
+              "Storage access has been granted for ${root.getStorageId(requireContext())}",
+              Toast.LENGTH_SHORT
+            )
+            .show()
+        }
+
+        override fun onStoragePermissionDenied(requestCode: Int) {
+          requestStoragePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+      }
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    storage.onSaveInstanceState(outState)
+    super.onSaveInstanceState(outState)
   }
 
   /** Initializes an empty repository in the app's private directory */
@@ -81,14 +136,14 @@ class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
     settings.edit { putBoolean(PreferenceKeys.GIT_EXTERNAL, true) }
     val externalRepo = settings.getString(PreferenceKeys.GIT_EXTERNAL_REPO)
     if (externalRepo == null) {
-      externalDirectorySelectAction.launch(directorySelectIntent)
+      storage.requestStorageAccess(REQUEST_CODE_STORAGE_ACCESS)
     } else {
       MaterialAlertDialogBuilder(requireActivity())
         .setTitle(resources.getString(R.string.directory_selected_title))
         .setMessage(resources.getString(R.string.directory_selected_message, externalRepo))
         .setPositiveButton(resources.getString(R.string.use)) { _, _ -> initializeRepositoryInfo() }
         .setNegativeButton(resources.getString(R.string.change)) { _, _ ->
-          repositoryInitAction.launch(directorySelectIntent)
+          repositoryInitAction.launch(null)
         }
         .show()
     }
@@ -151,6 +206,8 @@ class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
   }
 
   companion object {
+
+    private const val REQUEST_CODE_STORAGE_ACCESS = 100
 
     fun newInstance(): RepoLocationFragment = RepoLocationFragment()
   }
